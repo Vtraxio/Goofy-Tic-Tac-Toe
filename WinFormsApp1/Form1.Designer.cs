72ac0ce7f92f3bfc101b29ccfe287abb597c41ff
Vtraxio <55654178+Vtraxio@users.noreply.github.com>
2024-09-12 21:04:42 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            status = new Label();
+            buttonsLayout = new TableLayoutPanel();
+            button1 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            buttonsLayout.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // status
+            // 
+            status.Anchor = AnchorStyles.Bottom;
+            status.AutoSize = true;
+            status.Location = new Point(235, 399);
+            status.Name = "status";
+            status.Size = new Size(38, 15);
+            status.TabIndex = 10;
+            status.Text = "label1";
+            // 
+            // buttonsLayout
+            // 
+            buttonsLayout.ColumnCount = 3;
+            buttonsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            buttonsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            buttonsLayout.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            buttonsLayout.Controls.Add(button1, 0, 0);
+            buttonsLayout.Controls.Add(button9, 2, 2);
+            buttonsLayout.Controls.Add(button8, 1, 2);
+            buttonsLayout.Controls.Add(button7, 0, 2);
+            buttonsLayout.Controls.Add(button2, 1, 0);
+            buttonsLayout.Controls.Add(button3, 2, 0);
+            buttonsLayout.Controls.Add(button4, 0, 1);
+            buttonsLayout.Controls.Add(button6, 2, 1);
+            buttonsLayout.Controls.Add(button5, 1, 1);
+            buttonsLayout.Dock = DockStyle.Fill;
+            buttonsLayout.Location = new Point(3, 3);
+            buttonsLayout.Name = "buttonsLayout";
+            buttonsLayout.RowCount = 3;
+            buttonsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 33.33333F));
+            buttonsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            buttonsLayout.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            buttonsLayout.Size = new Size(502, 393);
+            buttonsLayout.TabIndex = 9;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Font = new Font("Segoe UI", 24F);
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(161, 125);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += MainButtonClick;
+            // 
+            // button9
+            // 
+            button9.Dock = DockStyle.Fill;
+            button9.Font = new Font("Segoe UI", 24F);
+            button9.Location = new Point(337, 265);
+            button9.Name = "button9";
+            button9.Size = new Size(162, 125);
+            button9.TabIndex = 6;
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += MainButtonClick;
+            // 
+            // button8
+            // 
+            button8.Dock = DockStyle.Fill;
+            button8.Font = new Font("Segoe UI", 24F);
+            button8.Location = new Point(170, 265);
+            button8.Name = "button8";
+            button8.Size = new Size(161, 125);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += MainButtonClick;
+            // 
+            // button7
+            // 
+            button7.Dock = DockStyle.Fill;
+            button7.Font = new Font("Segoe UI", 24F);
+            button7.Location = new Point(3, 265);
+            button7.Name = "button7";
+            button7.Size = new Size(161, 125);
+            button7.TabIndex = 8;
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += MainButtonClick;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Font = new Font("Segoe UI", 24F);
+            button2.Location = new Point(170, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(161, 125);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += MainButtonClick;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Font = new Font("Segoe UI", 24F);
+            button3.Location = new Point(337, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(162, 125);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += MainButtonClick;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Fill;
+            button4.Font = new Font("Segoe UI", 24F);
+            button4.Location = new Point(3, 134);
+            button4.Name = "button4";
+            button4.Size = new Size(161, 125);
+            button4.TabIndex = 5;
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += MainButtonClick;
+            // 
+            // button6
+            // 
+            button6.Dock = DockStyle.Fill;
+            button6.Font = new Font("Segoe UI", 24F);
+            button6.Location = new Point(337, 134);
+            button6.Name = "button6";
+            button6.Size = new Size(162, 125);
+            button6.TabIndex = 3;
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += MainButtonClick;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Fill;
+            button5.Font = new Font("Segoe UI", 24F);
+            button5.Location = new Point(170, 134);
+            button5.Name = "button5";
+            button5.Size = new Size(161, 125);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += MainButtonClick;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 1;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Controls.Add(status, 0, 1);
+            tableLayoutPanel2.Controls.Add(buttonsLayout, 0, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(0, 0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle());
+            tableLayoutPanel2.Size = new Size(508, 414);
+            tableLayoutPanel2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(508, 414);
+            Controls.Add(tableLayoutPanel2);
+            MinimumSize = new Size(250, 250);
+            Name = "Form1";
+            Text = "Form1";
+            buttonsLayout.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label status;
+        private TableLayoutPanel buttonsLayout;
+        private Button button1;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button6;
+        private Button button5;
+        private TableLayoutPanel tableLayoutPanel2;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+namespace WinFormsApp1 {
+	public partial class Form1 : Form {
+		private new enum Move {
+			None = 0,
+			X,
+			O
+		}
+
+		private          Move         _currentMove = Move.None;
+		private readonly List<Button> _buttons;
+
+		// Nie wyczytujemy stanu gry z przycisków, tylko z tej tabeli
+		private readonly Move[] _moves = new Move[9];
+
+		// Wygrywalne pozycje na planszy 0..=8 (indeksy)
+		private readonly int[][] _winPos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+		public Form1() {
+			InitializeComponent();
+			// Lista przycisków, aby mo¿na by³o otrzymaæ ich index (FindIndex)
+			_buttons = [button1, button2, button3, button4, button5, button6, button7, button8, button9];
+			Reset();
+		}
+
+		private void MainButtonClick(object sender, EventArgs e) {
+			var button   = (Button)sender;
+			var currText = button.Text;
+
+			if (currText != "") { // Jest zape³niony
+				return;
+			}
+
+			button.Text = _currentMove == Move.X ? "X" : "O";
+			var btnIndex = _buttons.FindIndex((x) => x.Name == button.Name);
+			_moves[btnIndex] = _currentMove;
+
+			if (_moves.All(x => x != Move.None)) { // Remis, wszystkie pola s¹ wype³nione (nie s¹ puste)
+				MessageBox.Show("Remis");
+				Reset();
+				return;
+			}
+
+			if (CheckWin())
+				return;
+
+			_currentMove = _currentMove == Move.X ? Move.O : Move.X;
+			status.Text  = _currentMove == Move.X ? "Ruch X" : "Ruch O";
+		}
+
+		// Bombowy "algorytm", który kiedyœ znalaz³em,
+		// w 2 klasie u¿y³em go do kó³ka i krzy¿yk na stronach internetowych
+		// wieæ postanowi³em ponownie tu go wstawiæ, ¿eby nie robiæ milliard ifów
+		// True-wygrana; False-kontynuuj
+		private bool CheckWin() {
+			foreach (var pos in _winPos) {
+				if (_moves[pos[0]] == _currentMove && _moves[pos[1]] == _currentMove && _moves[pos[2]] == _currentMove) {
+					foreach (var btnIndex in pos) {
+						_buttons[btnIndex].BackColor = Color.DarkOrange;
+					}
+
+					MessageBox.Show(_currentMove == Move.X ? "X wygrywa" : "O wygrywa");
+
+					Reset();
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		private void Reset() {
+			_currentMove = Move.O;
+			status.Text  = "Ruch O";
+
+			for (var j = 0; j < _buttons.Count; j++) {
+				_buttons[j].Text      = "";
+				_buttons[j].BackColor = DefaultBackColor;
+				_moves[j]             = Move.None;
+			}
+		}
+	}
+}
